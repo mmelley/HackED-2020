70e61740fa4991aafac1e82f18df1ed45486838e
mmelley <mmelley@ualberta.ca>
2020-01-17 15:25:26 -0700

    made the sample code cooler

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 import requests
-import sys
+import json
+import webbrowser
 
 #This is a sample API request
-response = requests.get("http://api.open-notify.org/astros.json")
-print(response.json())
+potd = requests.get("https://api.nasa.gov/planetary/apod", params={'api_key':'Ucbi3Q7hlGh9PulKWimpUVVHrOWyMiXdy5SYeaCN','hd':'true'})
+
+potd_json = potd.json()
+for i in potd_json:
+    print("%s: %s" % (i, potd_json[i]))
+
+webbrowser.open_new(potd_json['url'])
