6812825c90a51b279db04b0c3128688ed27a8988
Emily Skousbol <skousbol@ualberta.ca>
2020-01-18 15:25:32 -0700

    Last text message commit

--- message-sender.py
+++ message-sender.py
@@ -2,16 +2,16 @@ import requests
 import numpy
 import json
 from twilio.rest import Client
+import tkinter
 message = input("What would you like to send?")
 destination = "+17808686226"
-
+#Code snippet taken from
+# https://medium.com/quick-code/absolute-beginners-guide-to-slaying-apis-using-python-7b380dc82236
 def send_message(Message, destination):
     #Sends a text message to specified location
     account_sid = 'ACd2a9214e43319d6acef9351f1ce1f5f6'
     auth_token = 'a737cee82ec01b87cd58a8592c15e99b'
     client = Client(account_sid, auth_token)
-    r = requests.get('http://api.open-notify.org/astros.json')
-    people = r.json()
     #formulate the message that will be sent
     message = client.messages.create(
         to=destination,
